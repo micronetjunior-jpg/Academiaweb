66069e901190c6361ab8b856ffae22b3def593a9
Domentia <micronetjunior@icloud.com>
2026-03-30 07:33:44 +0000

    ok

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PUERTO = 80;
 
 // Servir archivos estáticos desde la carpeta "public"
-app.use(express.static("public"));
+app.use(express.static("app/public"));
 
 app.listen(PUERTO, () => {
   console.log(`Servidor corriendo en http://localhost:${PUERTO}`);
